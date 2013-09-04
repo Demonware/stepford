ae4ec1f83cdd1783bff8cc50a7df7b2d0889581b
Demian Brecht <dbrecht@demonware.net>
2013-09-04 08:00:53 -0700

    added setup and CHANGES

---
+++ setup.py
@@ -0,0 +1,32 @@
+from os.pth import abspath, dirname, join
+from setuptools import setup
+
+here = abspath(dirname(__file__))
+README = '\n'.join([open(join(here, 'README')).read(),
+    open(join(here, 'CHANGES')).read())
+
+requires = []
+
+setup(name='stepford',
+    keywords='python,facebook,integration test,test users',
+    version='0.1',
+    description='An implementation of the Facebook test user API',
+    author='Demian Brecht',
+    author_email='demianbrecht@gmail.com',
+    url='https://github.com/demianbrecht/stepford',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+    ],
+    long_description=README,
+    install_requires=requires,
+    packages=['stepford'],
+)
